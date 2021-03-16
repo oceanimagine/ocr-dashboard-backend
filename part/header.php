5f1547119815ef11c85a2af9e533c39b5311cb2a
845965 <845965@telkom.co.id>
2021-03-16 11:45:47 +0700

    Baru Baru

--- part/header.php
+++ part/header.php
@@ -20,20 +20,20 @@
                 <a href="#" class="dropdown-item">
                     <!-- Message Start -->
                     <div class="media">
-                        <img src="dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+                        <img src="dist/img/Fajrin.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
                         <div class="media-body">
                             <h3 class="dropdown-item-title">
-                                Brad Diesel
+                                Prayogi Fajrin
                                 <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
                             </h3>
-                            <p class="text-sm">Call me whenever you can...</p>
-                            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                            <p class="text-sm">Welcome Aboard.</p>
+                            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> a few minute ago</p>
                         </div>
                     </div>
                     <!-- Message End -->
                 </a>
                 <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+                <a href="index.php" class="dropdown-item dropdown-footer">Home</a>
             </div>
         </li>
         <!-- Notifications Dropdown Menu -->
@@ -45,12 +45,12 @@
                 <span class="dropdown-item dropdown-header">1 Notifications</span>
                 <div class="dropdown-divider"></div>
                 <a href="#" class="dropdown-item">
-                    <i class="fas fa-envelope mr-2"></i> 4 new messages
-                    <span class="float-right text-muted text-sm">3 mins</span>
+                    <i class="fas fa-envelope mr-2"></i> Welcome Aboard.
+                    <span class="float-right text-muted text-sm">1 min</span>
                 </a>
                 <div class="dropdown-divider"></div>
 
-                <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+                <a href="index.php" class="dropdown-item dropdown-footer">Home</a>
             </div>
         </li>
     </ul>
