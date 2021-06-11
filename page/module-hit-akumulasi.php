5cb9f0ef438417a634041a94277bb961dac7c391
845965 <845965@telkom.co.id>
2021-06-11 10:25:22 +0700

    Perbaikan Highchart ID Element.

--- page/module-hit-akumulasi.php
+++ page/module-hit-akumulasi.php
@@ -19,11 +19,11 @@ $end = new DateTime(date("Y-m-d"));
     $jumlah_akumulasi_ijazah = 0;
     $jumlah_akumulasi_all = 0;
     for($i = $begin; $i <= $end; $i->modify('+1 day')){
-        $query_jumlah_ktp = mysqli_query($connect, "select count(id_document) jumlah_hit_ktp from tbl_id_document_collection_log_backup where tipe_document = 'KTP' and DATE_FORMAT(timestamp, '%Y-%m-%d') = '".$i->format("Y-m-d")."'");
+        $query_jumlah_ktp = mysqli_query($connect, "select count(id_document) jumlah_hit_ktp from tbl_id_document_collection_log where tipe_document = 'KTP' and DATE_FORMAT(timestamp, '%Y-%m-%d') = '".$i->format("Y-m-d")."'");
         $hasil_jumlah_ktp = mysqli_fetch_array($query_jumlah_ktp);
-        $query_jumlah_ijazah = mysqli_query($connect, "select count(id_document) jumlah_hit_ijazah from tbl_id_document_collection_log_backup where tipe_document = 'IJAZAH' and DATE_FORMAT(timestamp, '%Y-%m-%d') = '".$i->format("Y-m-d")."'");
+        $query_jumlah_ijazah = mysqli_query($connect, "select count(id_document) jumlah_hit_ijazah from tbl_id_document_collection_log where tipe_document = 'IJAZAH' and DATE_FORMAT(timestamp, '%Y-%m-%d') = '".$i->format("Y-m-d")."'");
         $hasil_jumlah_ijazah = mysqli_fetch_array($query_jumlah_ijazah);
-        $query_jumlah_all = mysqli_query($connect, "select count(id_document) jumlah_hit_all from tbl_id_document_collection_log_backup where DATE_FORMAT(timestamp, '%Y-%m-%d') = '".$i->format("Y-m-d")."'");
+        $query_jumlah_all = mysqli_query($connect, "select count(id_document) jumlah_hit_all from tbl_id_document_collection_log where DATE_FORMAT(timestamp, '%Y-%m-%d') = '".$i->format("Y-m-d")."'");
         $hasil_jumlah_all = mysqli_fetch_array($query_jumlah_all);
         $jumlah_akumulasi_ktp = $jumlah_akumulasi_ktp + (int) $hasil_jumlah_ktp['jumlah_hit_ktp'];
         $jumlah_akumulasi_ijazah = $jumlah_akumulasi_ijazah + (int) $hasil_jumlah_ijazah['jumlah_hit_ijazah'];
